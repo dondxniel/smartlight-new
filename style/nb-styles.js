75ffe499c017c9464722624e10ec2a195d24835c
Jrcity <redemptionjonathan1@gmail.com>
2022-06-18 13:59:34 +0100

    ...

--- style/nb-styles.js
+++ style/nb-styles.js
@@ -347,6 +347,9 @@ export default {
   },
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
