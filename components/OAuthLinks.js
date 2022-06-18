03a7a227ce3292791bb47072339c02bb1d3eeea0
Daniel Don <dondaniel028@gmail.com>
2022-06-18 13:28:26 +0100

    Fix to open auth links

--- components/OAuthLinks.js
+++ components/OAuthLinks.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { View, Text, Card, Box } from 'native-base';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import nbStyles from '../style/nb-styles';
+import React from "react";
+import { View, Text } from "native-base";
+import { TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import nbStyles from "../style/nb-styles";
 
 const OAuthLinks = () => {
-    return (
-        <View>
-            <Text>Or continue with</Text>
-            <View {...nbStyles.oauthContainer}>
-                <Box {...nbStyles.oauthLink}>
-                    <Icon name='google' size={30} color={'#E0453A'} />
-                </Box>
+	return (
+		<View>
+			<Text>Or continue with</Text>
+			<View {...nbStyles.oauthContainer}>
+				<TouchableOpacity {...nbStyles.oauthLink}>
+					<Icon name="google" size={30} color={"#E0453A"} />
+				</TouchableOpacity>
 
-                <Box {...nbStyles.oauthLink}>
-                    <Icon name='apple' size={30} color={'#000'} />
-                </Box>
+				<TouchableOpacity {...nbStyles.oauthLink}>
+					<Icon name="apple" size={30} color={"#000"} />
+				</TouchableOpacity>
 
-                <Box {...nbStyles.oauthLink}>
-                    <Icon name='facebook-square' size={30} color={'#1977F0'} />
-                </Box>
-            </View>
-        </View>
-    )
-}
+				<TouchableOpacity {...nbStyles.oauthLink}>
+					<Icon name="facebook-square" size={30} color={"#1977F0"} />
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+};
 
 export default OAuthLinks;
